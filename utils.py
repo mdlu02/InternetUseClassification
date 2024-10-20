2ce036a9cd3fde4d02c8b88568797de9321b2046
mdlu02 <michaeldavidlu@gmail.com>
2024-10-20 19:35:06 +0000

    finished for midterm

--- utils.py
+++ utils.py
@@ -6,9 +6,10 @@ import pingouin as pg
 from tqdm import tqdm
 from dataclasses import dataclass
 from typing import List, Set, Dict
-from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
+from sklearn.preprocessing import (
+    MinMaxScaler, StandardScaler, OneHotEncoder, OrdinalEncoder
+)
 from sklearn.model_selection import train_test_split, StratifiedKFold
 
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -16,6 +17,18 @@ warnings.filterwarnings("ignore", category=UserWarning)
 SEED = 42
 
 PCA_CUTOFF = 0.95
+
+ORDINAL_FEATURES = {
+    "FGC-FGC_CU_Zone": [-1, 0, 1],
+    "FGC-FGC_GSND_Zone": [-1, 1, 2, 3],
+    "FGC-FGC_GSD_Zone": [-1, 1, 2, 3],
+    "FGC-FGC_PU_Zone": [-1, 0, 1],
+    "FGC-FGC_SRR_Zone": [-1, 0, 1],
+    "FGC-FGC_TL_Zone": [-1, 0, 1],
+    "BIA-BIA_Activity_Level_num": [-1, 1, 2, 3, 4, 5],
+    "BIA-BIA_Frame_num": [-1, 1, 2, 3],
+    "PreInt_EduHx-computerinternet_hoursday": [-1, 0, 1, 2, 3]
+}
 
 AGE_BUCKETS = {
     0: {5, 6, 7, 8, 9},
@@ -143,7 +156,7 @@ def cont_cols_to_exclude(data: pd.DataFrame, corr_threshold: float = 0.8) -> Set
 class SplitData:
     train_X: List[pd.DataFrame]
     val_X: List[pd.DataFrame]
-    test_X:  List[pd.Series]
+    test_X:  pd.DataFrame
     train_y: pd.Series
     val_y: pd.Series
     test_y: pd.Series
@@ -158,20 +171,21 @@ class FoldData:
     val_y: pd.Series
     test_y: pd.Series
     to_exclude: List[str]
-    pipeline: Pipeline
+    to_include: List[str]
+    pipeline: ColumnTransformer
 
 
 def split(
     data: pd.DataFrame,
     test_size: float = 0.2,
-    num_folds: int = 5,
+    num_folds: int = 3,
     seed: int = SEED
 ) -> SplitData:
   # Group by existence of timeseries data and stratify each group by `sii`
     print("Splitting data")
     original_size = len(data)
     data.dropna(subset=["sii"], inplace=True)
-    print(f"Dropped {original_size - len(data)} rows with missing `sii` data")
+    print(f"Dropped {original_size - len(data)} rows out of {original_size} with missing `sii` data")
 
     train_X, val_X = [], []
     train_y, val_y = [], []
@@ -188,7 +202,8 @@ def split(
     data["stratify_target"] = (
         data["sii"].astype(str) + "|"
         + data["has_timeseries"].astype(str) + "|"
-        + data["age_group"].astype(str)
+        + data["age_group"].astype(str) + "|"
+        + data["Basic_Demos-Sex"].astype(str)
     )
     stratify_counts = data["stratify_target"].value_counts()
 
@@ -230,7 +245,7 @@ def split(
         assert len(test_X) == len(test_y)
 
         assert len(train_X[fold]) + len(val_X[fold]) + len(test_X) == len(X)
-        assert abs(len(train_X[fold]) - (1 - 1 / num_folds)**2 * len(X)) < 2
+        assert abs(len(train_X[fold]) - (1 - 1 / num_folds) * len(X) * (1 - test_size)) < 2
 
     return SplitData(
         train_X=train_X,
@@ -242,36 +257,36 @@ def split(
     )
 
 
-def gen_timeseries_summary_data(df):
+def gen_timeseries_summary_data(df, t):
     summary_data = []
-
+    print(f"Generating timeseries summary data for {t}")
     for id, group in df.groupby("id"):
         summary_data.append({
             "id": id,
-            "light_median": group["light"].median(),
-            "light_std": group["light"].std(),
-            "light_min": group["light"].min(),
-            "light_max": group["light"].max(),
-            "light_25": group["light"].quantile(0.25),
-            "light_50": group["light"].quantile(0.50),
-            "light_75": group["light"].quantile(0.75),
-            "light_90": group["light"].quantile(0.90),
-            "anglez_median": group["anglez"].median(),
-            "anglez_std": group["anglez"].std(),
-            "anglez_min": group["anglez"].min(),
-            "anglez_max": group["anglez"].max(),
-            "anglez_25": group["anglez"].quantile(0.25),
-            "anglez_50": group["anglez"].quantile(0.50),
-            "anglez_75": group["anglez"].quantile(0.75),
-            "anglez_90": group["anglez"].quantile(0.90),
-            "enmo_median": group["enmo"].median(),
-            "enmo_std": group["enmo"].std(),
-            "enmo_min": group["enmo"].min(),
-            "enmo_max": group["enmo"].max(),
-            "enmo_25": group["enmo"].quantile(0.25),
-            "enmo_50": group["enmo"].quantile(0.50),
-            "enmo_75": group["enmo"].quantile(0.75),
-            "enmo_90": group["enmo"].quantile(0.90),
+            f"{t}_light_median": group["light"].median(),
+            f"{t}_light_std": group["light"].std(),
+            f"{t}_light_min": group["light"].min(),
+            f"{t}_light_max": group["light"].max(),
+            f"{t}_light_25": group["light"].quantile(0.25),
+            f"{t}_light_50": group["light"].quantile(0.50),
+            f"{t}_light_75": group["light"].quantile(0.75),
+            f"{t}_light_90": group["light"].quantile(0.90),
+            f"{t}_anglez_median": group["anglez"].median(),
+            f"{t}_anglez_std": group["anglez"].std(),
+            f"{t}_anglez_min": group["anglez"].min(),
+            f"{t}_anglez_max": group["anglez"].max(),
+            f"{t}_anglez_25": group["anglez"].quantile(0.25),
+            f"{t}_anglez_50": group["anglez"].quantile(0.50),
+            f"{t}_anglez_75": group["anglez"].quantile(0.75),
+            f"{t}_anglez_90": group["anglez"].quantile(0.90),
+            f"{t}_enmo_median": group["enmo"].median(),
+            f"{t}_enmo_std": group["enmo"].std(),
+            f"{t}_enmo_min": group["enmo"].min(),
+            f"{t}_enmo_max": group["enmo"].max(),
+            f"{t}_enmo_25": group["enmo"].quantile(0.25),
+            f"{t}_enmo_50": group["enmo"].quantile(0.50),
+            f"{t}_enmo_75": group["enmo"].quantile(0.75),
+            f"{t}_enmo_90": group["enmo"].quantile(0.90),
         })
 
     return pd.DataFrame(summary_data).reset_index(drop=True)
@@ -287,7 +302,7 @@ def read_timeseries(file_name, dir_name):
     return df
 
 
-def preprocess(data: SplitData) -> SplitData:
+def preprocess_folds(data: SplitData) -> SplitData:
     """
     for each training fold, preprocess as follows:
     - Remove highly correlated continuous features and save the list
@@ -311,28 +326,24 @@ def preprocess(data: SplitData) -> SplitData:
             results.append(read_timeseries(
                 file_name, f"data/series_train.parquet"
             ))
-        if len(results) > 100:
-            break
-    timeseries_df = pd.concat(results)
-    del results
-    # Getting summary stats for timeseries split by weekday and weekend
-    print("Generating timeseries summary data")
-    timeseries_df = timeseries_df[
+
+    timeseries_df = pd.concat(results)[
         ["id", "daytime", "weekday",
          "light", "anglez", "enmo", "non-wear_flag"]
     ]
     timeseries_df = timeseries_df[timeseries_df["non-wear_flag"] == 0]
-    weekday_df = timeseries_df[timeseries_df["weekday"].isin([0, 1, 2, 3, 4])]
-    weekend_df = timeseries_df[timeseries_df["weekday"].isin([5, 6])]
+    del results
+    # Getting summary stats for timeseries split by weekday and weekend
+    weekday_df = gen_timeseries_summary_data(
+        timeseries_df[timeseries_df["weekday"].isin([0, 1, 2, 3, 4])],
+        "weekday"
+    )
+    weekend_df = gen_timeseries_summary_data(
+        timeseries_df[timeseries_df["weekday"].isin([5, 6])],
+        "weekend"
+    )
 
-    weekday_df = gen_timeseries_summary_data(weekday_df)
-    weekend_df = gen_timeseries_summary_data(weekend_df)
-
-    for col in weekday_df.columns:
-        if col != "id":
-            weekday_df.rename(columns={col: f"weekday_{col}"}, inplace=True)
-            weekend_df.rename(columns={col: f"weekend_{col}"}, inplace=True)
-    timeseries_df = pd.merge(timeseries_df, weekday_df, on="id")
+    timeseries_df = pd.merge(weekend_df, weekday_df, on="id")
 
     data_types: Dict[str, List[str]] = get_data_types()
     for i in tqdm(range(len(data.train_X)), desc="Preprocessing folds"):
@@ -370,18 +381,24 @@ def preprocess(data: SplitData) -> SplitData:
             timeseries_df[timeseries_df["id"].isin(data.test_X["id"])],
             on="id"
         )
-        train_X.drop(columns=timeseries_to_exclude, inplace=True, errors="ignore")
-        val_X.drop(columns=timeseries_to_exclude, inplace=True, errors="ignore")
-        test_X.drop(columns=timeseries_to_exclude, inplace=True, errors="ignore")
+        train_X.drop(columns=timeseries_to_exclude + ["id"], inplace=True, errors="ignore")
+        val_X.drop(columns=timeseries_to_exclude + ["id"], inplace=True, errors="ignore")
+        test_X.drop(columns=timeseries_to_exclude + ["id"], inplace=True, errors="ignore")
 
         # Initialize pipeline
         filtered_cont_columns = [
             col for col in train_X.columns if col not in data_types["cat_columns"]
         ]
         one_hot = [
-            col for col in train_X.columns if col in data_types["cat_columns"]
+            col for col in train_X.columns
+            if col in data_types["cat_columns"] and col not in ORDINAL_FEATURES
         ]
-        min_max, std_scaler = [], []
+        ordinal = [
+            col for col in train_X.columns
+            if col in data_types["cat_columns"] and col in ORDINAL_FEATURES
+        ]
+
+        min_max, std_scaler = [], [], 
         for col in filtered_cont_columns:
             if "anglez_" in col:
                 min_max.append(col)
@@ -389,15 +406,43 @@ def preprocess(data: SplitData) -> SplitData:
                 one_hot.append(col)
             else:
                 std_scaler.append(col)
-        
-        pipeline = Pipeline([
+
+        # Fill missing values
+        for col in min_max:
+            mean = train_X[col].mean()
+            train_X[col] = train_X[col].fillna(mean)
+            val_X[col] = val_X[col].fillna(mean)
+            test_X[col] = test_X[col].fillna(mean)
+        for col in std_scaler:
+            median = train_X[col].median()
+            train_X[col] = train_X[col].fillna(median)
+            val_X[col] = val_X[col].fillna(median)
+            test_X[col] = test_X[col].fillna(median)
+        for cols in [one_hot, ordinal]:
+            for col in cols:
+                if train_X[col].dtype in ["int64", "float64"]:
+                    train_X[col] = train_X[col].fillna(-1)
+                    val_X[col] = val_X[col].fillna(-1)
+                    test_X[col] = test_X[col].fillna(-1)
+                elif train_X[col].dtype == "O":
+                    train_X[col] = train_X[col].fillna("missing")
+                    val_X[col] = val_X[col].fillna("missing")
+                    test_X[col] = test_X[col].fillna("missing")
+
+        transformers = [
             ("min_max", MinMaxScaler(), min_max),
             ("std_scaler", StandardScaler(), std_scaler),
-            ("one_hot", OneHotEncoder(), one_hot)
-        ])
+            ("one_hot", OneHotEncoder(sparse_output=False, handle_unknown="ignore"), one_hot)
+        ]
+        if len(ordinal) > 0:
+            values = [ORDINAL_FEATURES[col] for col in ordinal]
+            transformers.append(
+                ("ordinal", OrdinalEncoder(categories=values), ordinal)
+            )  
+        
+        pipeline = ColumnTransformer(transformers)
 
         # Fit pipeline
-        print(train_X.columns, min_max, std_scaler, one_hot)
         train_X = pipeline.fit_transform(train_X)
         val_X = pipeline.transform(val_X)
         test_X = pipeline.transform(test_X)
@@ -411,10 +456,8 @@ def preprocess(data: SplitData) -> SplitData:
                 val_y=data.val_y,
                 test_y=data.test_y,
                 to_exclude=to_exclude + timeseries_to_exclude,
+                to_include=min_max + std_scaler + one_hot + ordinal,
                 pipeline=pipeline
             )
         )
     return out
-
-
-    
